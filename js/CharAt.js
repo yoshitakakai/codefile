27407702c17aca65ff5abb7c44d7ec331b2d1439
Yoshitaka Kai <yoshitakakai.net@gmail.com>
2024-12-17 11:51:29 +0900

    Add js.

---
+++ js/CharAt.js
@@ -0,0 +1,21 @@
+const searchWordText = document.querySelector('#search-word-input');
+const prefectureList = document.querySelectorAll('#prefecture-list button');
+
+searchWordText.addEventListener('keyup', () => {
+    const searchWord = searchWordText.value;
+    
+    prefectureList.forEach((element) => {
+        if (!searchWord || searchWord === '') {
+            element.classList.remove('hide');
+            return;
+        }
+
+        const prefectureName = element.dataset.name;
+        const phonetic = element.dataset.phonetic;
+        if (searchWord.charAt(0) === prefectureName.charAt(0) || searchWord.charAt(0) === phonetic.charAt(0)) {
+            element.classList.remove('hide');
+        } else {
+            element.classList.add('hide');
+        }
+    });
+});
